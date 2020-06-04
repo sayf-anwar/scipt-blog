db22c851c659778dc435a0c3a77db5b937b5cc22
Sayf <santriweb@gmail.com>
2020-06-04 23:50:15 +0700

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,64 @@
+{
+  "name": "Santri Mbois",
+  "short_name": "Santri Mbois",
+  "description": "Media Santri Nusantara",
+  "lang": "id-ID",
+  "display": "standalone",
+  "background_color": "#fff",
+  "start_url": "/",
+  "theme_color": "#4182ee",
+  "orientation": "portrait",
+  "icons": [
+    {
+      "src": "https://1.bp.blogspot.com/-lUpOxouc1WQ/XtZZiYMuyeI/AAAAAAAAHjc/MyFDwzjrLSIUgtkFrKNYI1J_oJ_y10WZgCNcBGAsYHQ/s1600/favicon-32x32.png",
+      "sizes": "32x32",
+      "type": "image/png",
+      "purpose": "any maskable"
+    },
+    {
+      "src": "https://1.bp.blogspot.com/-1c_sGtMcH6M/XtZZgrKGLGI/AAAAAAAAHjE/1Wfaq9frCRQRysFoCr9HqaWPnY-rOTlnQCNcBGAsYHQ/s1600/apple-touch-icon-72x72.png",
+      "sizes": "72x72",
+      "type": "image/png",
+      "purpose": "any maskable"
+    },
+    {
+      "src": "https://1.bp.blogspot.com/-JiyBqeEhfMw/XtZZiEIegLI/AAAAAAAAHjY/uZizA85I65cql7WrvwDz0PhtBYxUH501ACNcBGAsYHQ/s1600/favicon-96x96.png",
+      "sizes": "96x96",
+      "type": "image/png",
+      "purpose": "any maskable"
+    },
+    {
+      "src": "https://1.bp.blogspot.com/-MT8Zza9MlIM/XtZZhN2ZvJI/AAAAAAAAHjM/XsL3caH8-oMNvuWqOFxkWTRLNN3mkALhwCNcBGAsYHQ/s1600/favicon-128.png",
+      "sizes": "128x128",
+      "type": "image/png",
+      "purpose": "any maskable"
+    },
+    {
+      "src": "https://1.bp.blogspot.com/-bExYdY8EFuk/XtZZi8xtDRI/AAAAAAAAHjk/0cSMvJXxciUU9N1Zadn23gdha86PhqCyQCNcBGAsYHQ/s1600/mstile-144x144.png",
+      "sizes": "144x144",
+      "type": "image/png",
+      "purpose": "any maskable"
+    },
+    {
+      "src": "https://1.bp.blogspot.com/-egX9kgHUFhg/XtZZgK6X27I/AAAAAAAAHi4/nhaqOTIOtEsC-xba84KGNZM5gKMW_XLwgCNcBGAsYHQ/s1600/apple-touch-icon-152x152.png",
+      "sizes": "152x152",
+      "type": "image/png",
+      "purpose": "any maskable"
+    },
+    {
+      "src": "https://1.bp.blogspot.com/-NnAdOk5COdE/XpsS9TO6JII/AAAAAAAAHJ8/OWMlCWLiQvwv04xliI0lrlWL0g9Z57CdgCNcBGAsYHQ/s1600/android-icon-192x192.png",
+      "sizes": "192x192",
+      "type": "image/png",
+      "purpose": "any maskable"
+    },
+    {
+      "src": "https://1.bp.blogspot.com/-vjYA0136h44/XpsTiMGh4qI/AAAAAAAAHKE/sVTRRCcFpeYFh3Qj9_8519TxGI67xBz5ACNcBGAsYHQ/s1600/icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png",
+      "purpose": "any maskable"
+    }
+  ],
+  "serviceworker": {
+    "src": "./sw.js"
+  }
+}
